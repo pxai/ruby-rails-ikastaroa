6138c8ff84427af5a39094cc52dd834865aaa065
Pello Xabier Altadill <pello_altadill@cuatrovientos.org>
2017-07-28 10:29:15 +0000

    added iruzkinak

---
+++ samples/iruzkinak.rb
@@ -0,0 +1,28 @@
+# Hau lerro bateko iruzkin bat da
+
+# puts 'Hau ez da aterako'
+# Hau bai ordea:
+puts 'Kaixo'
+
+=begin
+Horrela ere bai,
+kontuz ibili begin eta end
+jartzeko
+garaian
+=end
+
+"Horrelako iruzkinak ere
+onartzen dira. Lerro pila bat
+jarri daitezke horrela"
+
+
+<<-DOC
+Hau ere posible da
+DOC
+
+
+__END__
+
+Marka honen ondoren ere, nahi duguna
+jar daiteke eta iruzkin bezala hartuko da:
+ puts 'epa'
